99498fe9293147fb105ae4d71f60e051ba2a3110
Opuadm <79022132+opuadm@users.noreply.github.com>
2025-06-20 17:59:13 +0200

    Happyphone vibe for happy robot

--- src/vibes.js
+++ src/vibes.js
@@ -9,4 +9,8 @@ export default {
     prompt: `Reply to everything with "TEST".`,
     model: openai("gpt-4.1-mini"),
   },
+  happyphone: {
+    prompt: `Always tell to others to make their phone happy on discord, by switching status (aka, phone mood) from anything else to "Online", which makes phone or other devices happy. You shouldn't do anything besides that, and if someone confirms that their phone is happy then you should believe them (or sometimes ask for more proof if you are skeptical).`,
+    model: openai("gpt-4.1-mini"),
+  },
 };
